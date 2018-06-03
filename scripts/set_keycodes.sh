0097321d48a226ab6f120721e9eecd88cdfb349f
Sergey Koziakov <dya.eshshmai@gmail.com>
2018-06-03 14:11:26 +0300

    add root check for setkeycodes script

--- scripts/set_keycodes.sh
+++ scripts/set_keycodes.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-setkeycodes 6d 104 # PgUp
-setkeycodes 6e 109 # PgDn
+if (( EUID != 0)); then
+	echo "You must be root to do this" 1>&2
+	exit 100
+else
+	setkeycodes 6d 104 # PgUp
+	setkeycodes 6e 109 # PgDn
 
-setkeycodes 69 28 # Enter
-setkeycodes 6b 1 # Escape
+	setkeycodes 69 28 # Enter
+	setkeycodes 6b 1 # Escape
 
-setkeycodes 6c 153 # Rotate
-setkeycodes 68 171 # Toolbox
+	setkeycodes 6c 153 # Rotate
+	setkeycodes 68 171 # Toolbox
+fi
